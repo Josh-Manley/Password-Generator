44d99684dbba9fc79a315b6e2bd46a7511341726
Josh-Manley <joshmanley45@gmail.com>
2024-01-12 00:06:46 -0700

    re-doing code - added confirms for upper case and lowercase if confirmed pushes functions that randomly select characters from specifed object property

--- script/script.js
+++ script/script.js
@@ -13,29 +13,15 @@ let typeOfFunction = [];
 
 let numberSelect = prompt("Choose password length (between 8 and 128 characters).");
 
-  // if all variables are unchecked alert user to check at least one box
-  if (number + symbol + upper + lower === 0) {
-    alert("No box checked. Please check at least one box.");
-    return;
-  }
-  // Write password to the #password input
-  let passwordText = document.querySelector("#password");
+let confirmUpperCase = confirm('Include uppercase characters');
+if (confirmUpperCase) {
+  typeOfFunction.push(() => characters.upperCase[Math.floor(Math.random() * characters.upperCase.length)]);
+}
 
-  let length = document.getElementById("length");
-
-  let password = "";
-  // while loop that stops once password length is equal to the length value
-  while(password.length < length.value) {
-    // randomly selects functions in the typeOfFunction array
-    let selectingFunction = typeOfFunction[Math.floor(Math.random() * typeOfFunction.length)];
-    // puts only checked functions in variable
-    let isChecked = document.getElementById(selectingFunction.name).checked;
-    // only checked checkboxes will be added to the password
-    if (isChecked) {
-      password += selectingFunction();
-    }
-  }
-  passwordText.innerHTML = password;
+let confirmLowerCase = confirm('Include lowercase characters');
+if (confirmLowerCase) {
+  typeOfFunction.push(() => characters.lowerCase[Math.floor(Math.random() * characters.lowerCase.length)]);
+}
 }
 
 // Get references to the #generate element
